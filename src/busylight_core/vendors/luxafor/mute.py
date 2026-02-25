8d47268dfda947b9bfe532a66ad8976782b67716
JnyJny <erik.oshaughnessy@gmail.com>
2026-02-25 13:09:15 -0600

    Bug: Luxafor Mute is_button method does not work.
    Need an async coroutine to monitor press/release events to update state of button.

--- src/busylight_core/vendors/luxafor/mute.py
+++ src/busylight_core/vendors/luxafor/mute.py
@@ -24,15 +24,24 @@ class Mute(Flag):
     @property
     def button_on(self) -> bool:
         """Return True if the mute button is currently pressed."""
+        # EJO The Mute device returns:
+        #     - [131, 0, 0, 0, 0, 0, 0, 0] on push
+        #     - [131, 1, 0, 0, 0, 0, 0, 0] on release
+        #
+        #     this device needs an async coroutine to update the
+        #     button state [pushed/released] since it's unlikely that
+        #     this property will be polled at the right time to catch
+        #     the button state change.
+        #
+        #     Sometimes there is a [66, 0, 0, 0, 0, 0, 0, 0] but I'll
+        #     have to refer to the documentation to figure out what
+        #     that signifies.
+        #
+        #     Until these issues are resolved, this method is busted.
+
         results = self.read_strategy(8, 200)
 
-        try:
-            if results[0] == 66:
-                self._button = False  # ???
-
-            if results[0] == 131:
-                return bool(results[1])
-        except IndexError:
-            pass
+        if results and results[0] == 131:
+            return bool(results[1])
 
         return False
